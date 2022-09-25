d116b321a9ae87a84b5ed66f361838b754f72883
Lesego Innocent <101153981+Lesego-phy@users.noreply.github.com>
2022-09-25 21:32:29 +0200

    Add files via upload

---
+++ AirbnbClone/src/screens/Type/index.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {View, Text, Pressable} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+
+import styles from './styles.js';
+
+const TypeScreen = (props) => {
+  const [text, onChangeText] = useState("");
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  return (
+    <View style={{justifyContent: 'space-between'}}>
+      <View style={styles.row}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder= 'type of event? '
+      />
+        <View style={styles.iconContainer}>
+             <FontAwesome name="group" size={25} color={'#4B0082'} />
+        </View>
+
+      </View>
+
+      <Pressable
+        onPress={() =>
+          navigation.navigate('Home', {
+            screen: 'Explore',
+            params: {
+              screen: 'SearchResults',
+              params: {
+                //eventtype: ,
+                viewport: route.params.viewport,
+              }
+            },
+          })
+        }
+        style={{
+          marginBottom: 20,
+          backgroundColor: '#f15454',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: 50,
+          marginHorizontal: 20,
+          borderRadius: 10,
+        }}>
+        <Text style={{fontSize: 20, color: 'white', fontWeight: 'bold'}}>
+          Search
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default GuestsScreen;
