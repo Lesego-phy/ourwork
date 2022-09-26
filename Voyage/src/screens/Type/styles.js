259e9a7594a6bceb04c9de989e49d7548989903a
Lesego Innocent <101153981+Lesego-phy@users.noreply.github.com>
2022-09-26 17:27:02 +0200

    Add files via upload

---
+++ Voyage/src/screens/Type/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 18
+  },
+
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+  },
+  iconContainer: {
+    backgroundColor: '#e7e7e7',
+    padding: 7,
+    borderRadius: 10,
+    marginRight: 15,
+  },
+});
+
+export default styles;
